0f90e29f1bf3df8f3640b4e4b459518749f686d4
val.istar.guo <val.istar.guo@gmail.com>
2024-03-13 09:03:58 +0800

    增加NpmPackageVersionDist

---
+++ app/backend/src/modules/registry/entity/npm-package-version-dist.entity.ts
@@ -0,0 +1,8 @@
+import { Entity } from '@mikro-orm/core'
+import { BaseEntity } from '~/entities/base.entity'
+
+@Entity()
+export class NpmPackageVersionDist extends BaseEntity {
+  tarball: string
+  integrity: string
+}
