4c163d7e5139212ff03faae181ff6fbe5968342d
val.istar.guo <val.istar.guo@gmail.com>
2024-05-05 16:02:45 +0800

    迁移脚本自动添加需要禁用的应用编号

--- app/backend/src/migrations/Migration20240428154537.ts
+++ app/backend/src/migrations/Migration20240428154537.ts
@@ -1,14 +1,39 @@
-import { Migration } from '@mikro-orm/migrations';
+/* eslint-disable @typescript-eslint/require-await */
+import { Migration } from '@mikro-orm/migrations'
+
+const forbiddenCodes = [
+  'buka',
+  'webpack',
+  'rollup',
+  'react',
+  'vue',
+  'angular',
+  'nestjs',
+  'next',
+  'types',
+  'nuxt',
+  'nuxtjs',
+  'tailwindcss',
+  'asyncapi',
+  'shikijs',
+  'vueuse',
+  'tabler',
+  'pinia',
+  'typescript-eslint',
+  'eslint',
+]
 
 export class Migration20240428154537 extends Migration {
-
   async up(): Promise<void> {
-    this.addSql('create table `forbidden_application_code` (`id` bigint unsigned not null auto_increment primary key comment \'主键\', `created_at` datetime not null default CURRENT_TIMESTAMP comment \'创建时间\', `updated_at` datetime not null default CURRENT_TIMESTAMP comment \'更新时间\', `code` varchar(255) not null comment \'唯一应用编码\') default character set utf8mb4 engine = InnoDB;');
-    this.addSql('alter table `forbidden_application_code` add unique `forbidden_application_code_code_unique`(`code`);');
+    this.addSql('create table `forbidden_application_code` (`id` bigint unsigned not null auto_increment primary key comment \'主键\', `created_at` datetime not null default CURRENT_TIMESTAMP comment \'创建时间\', `updated_at` datetime not null default CURRENT_TIMESTAMP comment \'更新时间\', `code` varchar(255) not null comment \'唯一应用编码\');')
+    this.addSql('alter table `forbidden_application_code` add unique `forbidden_application_code_code_unique`(`code`);')
+
+    for (const code of forbiddenCodes) {
+      this.addSql(`insert into \`forbidden_application_code\` (\`code\`) values ('${code}');`)
+    }
   }
 
   async down(): Promise<void> {
-    this.addSql('drop table if exists `forbidden_application_code`;');
+    this.addSql('drop table if exists `forbidden_application_code`;')
   }
-
 }
