234ee6df62e0bed567bce5ab65b1c1f196c8e01e
val.istar.guo <val.istar.guo@gmail.com>
2024-05-06 13:59:48 +0800

    修复无法迁移脚本无法运行的问题

---
+++ app/backend/src/constants/forbidden-application-codes.ts
@@ -0,0 +1,82 @@
+export const forbiddenApplicationCodes = [
+  {
+    code: 'buka',
+    description: '',
+  },
+  {
+    code: 'webpack',
+    description: 'https://webpack.js.org/',
+  },
+  {
+    code: 'rollup',
+    description: 'https://rollupjs.org/',
+  },
+  {
+    code: 'babel',
+    description: 'https://babeljs.io/',
+  },
+  {
+    code: 'react',
+    description: 'https://reactjs.org/',
+  },
+  {
+    code: 'vue',
+    description: 'https://vuejs.org/',
+  },
+  {
+    code: 'angular',
+    description: 'https://angular.io/',
+  },
+  {
+    code: 'nestjs',
+    description: 'https://nestjs.com/',
+  },
+  {
+    code: 'next',
+    description: 'https://nextjs.org/',
+  },
+  {
+    code: 'nuxt',
+    description: 'https://nuxtjs.org/',
+  },
+  {
+    code: 'nuxtjs',
+    description: 'https://nuxtjs.org/',
+  },
+  {
+    code: 'express',
+    description: 'https://expressjs.com/',
+  },
+  {
+    code: 'tailwindcss',
+    description: 'https://tailwindcss.com/',
+  },
+  {
+    code: 'asyncapi',
+    description: 'https://www.asyncapi.com/',
+  },
+  {
+    code: 'shikijs',
+    description: 'https://shiki.matsu.io/',
+  },
+  {
+    code: 'vueuse',
+    description: 'https://vueuse.org/',
+  },
+  {
+    code: 'pinia',
+    description: 'https://pinia.esm.dev/',
+  },
+  {
+    code: 'tabler',
+    description: 'https://tablericons.com/',
+  },
+  {
+    code: 'eslint',
+    description: 'https://eslint.org/',
+  },
+  {
+    code: 'typescript-eslint',
+    description: 'https://typescript-eslint.io/',
+  },
+]
--- app/backend/src/migrations/Migration20240505121024.ts
+++ app/backend/src/migrations/Migration20240505121024.ts
@@ -1,15 +1,13 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/require-await */
 import { Migration } from '@mikro-orm/migrations'
-
-const forbiddenCodes = require('./forbidden-application-codes.json')
+import { forbiddenApplicationCodes } from '../constants/forbidden-application-codes'
 
 
 export class Migration20240505121024 extends Migration {
   async up(): Promise<void> {
     this.addSql('alter table `forbidden_application_code` add `description` varchar(255) not null comment \'描述\';')
 
-    for (const { code, description } of forbiddenCodes) {
+    for (const { code, description } of forbiddenApplicationCodes) {
       this.addSql(`insert into \`forbidden_application_code\` (\`code\`, \`description\`) values (${JSON.stringify(code)}, ${JSON.stringify(description)});`)
     }
   }
