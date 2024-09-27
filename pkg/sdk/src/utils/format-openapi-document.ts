dcdb97a9fb8a0d8bc3bdcd49bd281e326d6e8c80
val.istar.guo <val.istar.guo@gmail.com>
2024-09-27 14:20:55 +0800

    style: 易错代码增加注释

--- pkg/sdk/src/utils/format-openapi-document.ts
+++ pkg/sdk/src/utils/format-openapi-document.ts
@@ -8,6 +8,13 @@ import { validateSwagger3 } from './validate-swagger3'
 export async function formatOpenapiDocument(document: OpenAPI.Document): Promise<OpenAPIV3.Document> {
   // 修复中文和特殊字符串
   const swagger = fixSwagger(document)
+
+  /**
+   * NOTE: fixSwagger 需要在 toSwagger3 之前运行
+   *       因为 toSwagger3 会将部分中文字符替换成随机数字
+   *       导致 fixSwagger 无法正确修复中文字符
+   */
+
   // 将 swagger2 转换为 swagger3
   const swagger3 = await toSwagger3(swagger)
   // 删除不必要的$ref，避免模板解析出错
